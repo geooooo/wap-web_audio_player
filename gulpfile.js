694e6aad9850dc5df32e5d564267686084b21927
geo <brainit@mail.ru>
2018-01-08 19:15:46 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var pug  = require('gulp-pug');
+var scss = require('gulp-sass');
+var ts   = require('gulp-typescript');
+
+
+gulp.task('compile-pug', function () {
+    return gulp.src('app/**/*.pug')
+               .pipe(pug())
+               .pipe(gulp.dest(function (file) {
+                   return file.base;
+               }));
+});
+
+
+gulp.task('compile-scss', function () {
+    return gulp.src('app/**/*.scss')
+               .pipe(scss()/*.on('error', function(){console.log("bad...");})*/)
+               .pipe(gulp.dest(function (file) {
+                   return file.base;
+               }));
+});
+
+
+gulp.task('compile-ts', function () {
+    return gulp.src('app/**/*.ts')
+               .pipe(ts())
+               .pipe(gulp.dest(function (file) {
+                   return file.base;
+               }));
+});
+
+
+gulp.task('compile', function () {
+    gulp.watch('app/**/*.pug',  gulp.series('compile-pug'));
+    gulp.watch('app/**/*.scss', gulp.series('compile-scss'));
+    gulp.watch('app/**/*.ts',   gulp.series('compile-ts'));
+});
